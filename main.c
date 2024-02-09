3e960e9670f53b4689cde47afdfef7dfef4880f4
vlad <ecscalibur_10@mail.ru>
2024-02-09 19:16:18 +0300

    lab_1

--- main.c
+++ main.c
@@ -4,22 +4,24 @@
 int main(void){
     dict *a = createDict();
 
-    add(a, "a", 1);
     add(a, "b", 2);
 
     add(a, "d", 4);
     add(a, "f", 5);
     add(a, "c", 5);
+    add(a, "a", 1);
 
 //    delByKey(a, "f");
-    delByKey(a, "a");
+//    delByKey(a, "a");
 
     add(a, "c", 4);
 //    add(a, "c", 6);
 
     keyValue *b = getByKey(a, "d");
 
-
-
     printDict(a);
+
+    delDict(a);
+
+    return 0;
 }
